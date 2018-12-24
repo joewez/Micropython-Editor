8ec49a12aacdeab3425697a1a8b2501e994409ff
hh <robert@hammelrath.com>
2018-12-24 13:18:15 +0100

    pye.py, pye2.py and derivates: Change Home/End key behavior
    Home and End always move to the start/end of line first, unless the cursor
    is at the first/last position of the line. In that case move to
    the first non-blank or before the end-of-line comment.

--- strip.sh
+++ strip.sh
@@ -8,7 +8,5 @@ cpp -D MICROPYTHON pye.py | sed "s/#.*$//" | sed "/^$/d" >pye_mp.py
 cat shebang <(cpp -D LINUX       pye.py | sed "s/#.*$//" | sed "/^$/d") >pye
 chmod +x pye
 mpy-cross -o pye_mp.mpy pye_mp.py
-#
-cpp -D MICROPYTHON pye_lazy.py | sed "s/#.*$//" | sed "/^$/d" >pye_mpl.py
 
 
